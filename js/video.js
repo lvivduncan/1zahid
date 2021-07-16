f67fe6c453623840c34ab1caa0101b60bd315247
lvivduncan <lvivduncan@gmail.com>
2021-07-16 13:52:09 +0300

    video test

--- js/video.js
+++ js/video.js
@@ -1,75 +1,22 @@
-function onYouTubeIframeAPIReady() {
+// Loads the YouTube IFrame API JavaScript code.
+var tag = document.createElement("script")
+tag.src = "https://www.youtube.com/iframe_api"
+// Inserts YouTube JS code into the page.
+var firstScriptTag = document.getElementsByTagName("script")[0]
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-    var player;
-  
-    player = new YT.Player('YouTubeBackgroundVideoPlayer', {
-  
-        videoId: '{$loadYouTubeVideoID}', // YouTube Video ID
-  
-        width: 1280,               // Player width (in px)
-  
-        height: 720,              // Player height (in px)
-  
-        playerVars: {
-  
-          playlist: '{$loadYouTubeVideoID}',
-  
-            autoplay: 1,        // Auto-play the video on load
-  
-            autohide: 1,
-  
-            disablekb: 1, 
-  
-            controls: 0,        // Hide pause/play buttons in player
-  
-            showinfo: 0,        // Hide the video title
-  
-            modestbranding: 1,  // Hide the Youtube Logo
-  
-            loop: 1,            // Run the video in a loop
-  
-            fs: 0,              // Hide the full screen button
-  
-            autohide: 0,         // Hide video controls when playing
-  
-            rel: 0,
-  
-            enablejsapi: 1
-  
-        },
-  
-        events: {
-  
-          onReady: function(e) {
-  
-              e.target.mute();
-  
-              e.target.setPlaybackQuality('hd1080');
-  
-          },
-  
-          onStateChange: function(e) {
-  
-            if(e && e.data === 1){
-  
-                var videoHolder = document.getElementById('home-banner-box');
-  
-                if(videoHolder && videoHolder.id){
-  
-                  videoHolder.classList.remove('loading');
-  
-                }
-  
-            }else if(e && e.data === 0){
-  
-              e.target.playVideo()
-  
-            }
-  
-          }
-  
-        }
-  
-    });
-  
-  }
+var player
+
+// onYouTubeIframeAPIReady() is called when the IFrame API is ready to go.
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    videoId: "LS-ErOKpO4E", // the ID of the video (mentioned above)
+    playerVars: {
+      autoplay: 1, // start automatically
+      controls: 0, // don't show the controls (we can't click them anyways)
+      modestbranding: 1, // show smaller logo
+      loop: 1, // loop when complete
+      playlist: "kNizPk7xBbs" // required for looping, matches the video ID
+    }
+  })
+}
